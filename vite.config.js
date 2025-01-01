965f2b19c4f66eb80acf79f60fe7c5c396b372af
Mahmoud-FullStackee <dev.mahmoud.adel@gmail.com>
2025-01-01 17:02:11 +0200

    Fix Preview Examples

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/country-cities-vite', 
+  base: '/country-cities-vite',
   // server: {
   //   hmr: false,
   // },
+  build: {
+    rollupOptions: {
+      input: {
+        index: './index.html', // Main index.html
+        'example-1': './example-1.html',
+        'example-2': './example-2.html',
+        'example-3': './example-3.html',
+        'example-4': './example-4.html',
+        'example-5': './example-5.html',
+      },
+    },
+  },
 });
